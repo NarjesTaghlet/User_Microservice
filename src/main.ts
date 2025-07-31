7004c97f3def8cbc58515b035f51d1544705ac93
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-07-31 08:40:04 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.enableCors({
      origin: [
     'http://localhost:4200',  // Dev local
-    'https://davbjddcsuel.cloudfront.net',
+    '*',
 
   ], // Autoriser tous les sous-domaines Ngrok // ✅ Autoriser uniquement le frontend Angular
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -24,7 +24,7 @@ async function bootstrap() {
   // Express CORS Middleware
 const corsMiddleware = (req, res, next) => {
   const allowedOrigins = [
-    'https://davbjddcsuel.cloudfront.net',
+    '*',
   ];
   
   const origin = req.headers.origin;
